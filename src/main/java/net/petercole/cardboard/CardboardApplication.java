87132b7ae136fb3368262a3890ce0ec96d1cb54b
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2020-05-11 15:03:58 +0100

    first commit - project setup with spring initializr

---
+++ src/main/java/net/petercole/cardboard/CardboardApplication.java
@@ -0,0 +1,13 @@
+package net.petercole.cardboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/petercole/cardboard/CardboardApplicationTests.java
@@ -0,0 +1,13 @@
+package net.petercole.cardboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
